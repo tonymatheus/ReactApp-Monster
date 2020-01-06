67fa6c3bcef2d1250a93309b70b93cf59e958758
tonymatheus <tony.chaves14@gmail.com>
2020-01-06 15:46:56 +0000

    Projeto Em Produção

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React ,{Component}from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {cardList} from './components/card-list/card-list.component';
+
+class App extends Component{
+  
+  constructor(){
+    super();
+
+    this.state= {
+      monsters:[]
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}));
+  }
+
+
+  render(){
+    return(
+      <div className="App">
+      {this.state.monsters.map(monster=> (
+        <h1 key={monster.id}>{monster.name}</h1>
+
+       ))}
+    </div>
+    );
+  }
+}
+
+export default App;
