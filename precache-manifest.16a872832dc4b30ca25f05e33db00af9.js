b2c17ffbbc74ee4ff1862dfb450f63c623dd8ce0
tonymatheus <tony.chaves14@gmail.com>
2020-01-10 12:52:12 +0000

    Updates

---
+++ precache-manifest.16a872832dc4b30ca25f05e33db00af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edab827d616d8656ca404f0e4cee4051",
+    "url": "/ReactApp-Monster/index.html"
+  },
+  {
+    "revision": "a1be83d52ea6ef75c947",
+    "url": "/ReactApp-Monster/static/css/main.c3f1f02c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/ReactApp-Monster/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactApp-Monster/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1be83d52ea6ef75c947",
+    "url": "/ReactApp-Monster/static/js/main.6aefdd83.chunk.js"
+  },
+  {
+    "revision": "d0b62e532fb7a3f4f19c",
+    "url": "/ReactApp-Monster/static/js/runtime-main.38a56ab3.js"
+  }
+]);
